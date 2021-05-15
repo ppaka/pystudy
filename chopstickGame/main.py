bf7c39a48c4a9b271aa8594be97f67a00bed6565
ppaka <asj0216@gmail.com>
2021-05-15 15:05:46 +0900

    non

--- chopstickGame/main.py
+++ chopstickGame/main.py
@@ -13,25 +13,43 @@ def game():
         print("GameOver")
     elif computer_cs_right >= 5:
         print("GameOver")
-    else:
-        get_player_input()
 
 
 def get_player_input():
-    hand = str(input("[당신의 왼손, 오른손 중에 하나를 입력해주세요]"))
+    hand = str(input("[플레이어1, 당신의 왼손, 오른손 중에 하나를 입력해주세요]"))
 
     if hand == "왼손":
-        atk_hand = str(input("[이제, 공격할 상대의 왼손, 오른손 중에 하나를 입력해주세요]"))
+        atk_hand = str(input("[플레이어1, 이제, 공격할 상대의 왼손, 오른손 중에 하나를 입력해주세요]"))
         if atk_hand == "왼손":
             add_cs("left", "left", "player1")
         elif atk_hand == "오른손":
             add_cs("left", "right", "player1")
     elif hand == "오른손":
-        atk_hand = str(input("[이제, 공격할 상대의 왼손, 오른손 중에 하나를 입력해주세요]"))
+        atk_hand = str(input("[플레이어1, 이제, 공격할 상대의 왼손, 오른손 중에 하나를 입력해주세요]"))
         if atk_hand == "왼손":
             add_cs("right", "left", "player1")
         elif atk_hand == "오른손":
             add_cs("right", "right", "player1")
+    else:
+        print("[올바른 입력이 아닙니다. 다시 입력해주세요]")
+        get_player_input()
+
+
+def get_second_player_input():
+    hand = str(input("[플레이어2, 당신의 왼손, 오른손 중에 하나를 입력해주세요]"))
+
+    if hand == "왼손":
+        atk_hand = str(input("[플레이어2, 이제, 공격할 상대의 왼손, 오른손 중에 하나를 입력해주세요]"))
+        if atk_hand == "왼손":
+            add_cs("left", "left", "player2")
+        elif atk_hand == "오른손":
+            add_cs("left", "right", "player2")
+    elif hand == "오른손":
+        atk_hand = str(input("[플레이어2, 이제, 공격할 상대의 왼손, 오른손 중에 하나를 입력해주세요]"))
+        if atk_hand == "왼손":
+            add_cs("right", "left", "player2")
+        elif atk_hand == "오른손":
+            add_cs("right", "right", "player2")
     else:
         print("[올바른 입력이 아닙니다. 다시 입력해주세요]")
         get_player_input()
@@ -66,3 +84,4 @@ def add_cs(selected_hand, target_hand, now_turn):
 
 
 get_player_input()
+get_second_player_input()
