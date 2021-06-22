7543b18a9d255dd5e4676927450c075d703be8cd
ppaka <asj0216@gmail.com>
2021-06-22 14:02:29 +0900

    주석추가

--- club study/class/practice3.py
+++ club study/class/practice3.py
@@ -1,13 +1,13 @@
 class Person:
-    def __init__(self, name, age, height):
+    def __init__(self, name, age, height):  # 생성자 메서드
         self.name = name
         self.age = age
         self.height = height
 
-    def __str__(self):
+    def __str__(self):   # 클래스가 문자열 타입으로 호출될 때 사용되는 메서드
         return "Person 설명, 이름은 " + self.name + " 키는" + str(self.height)
 
-    def __len__(self):
+    def __len__(self):   #
         return self.height
 
     def __getitem__(self, key):
